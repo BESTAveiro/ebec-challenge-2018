5afcc2927944a55426947f5be5071ac3b5ebf2a7
Diego Caldeira Hernandez <dc.hernandez@ua.pt>
2017-12-12 12:04:47 +0000

    ebec challenge aveiro 2018 1ra fase fixed

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= "http://".$_SERVER['HTTP_HOST']."/site-ebec-2017";
+$config['base_url']	= "http://".$_SERVER['HTTP_HOST']."/ebec-challenge";
 
 /*
 |--------------------------------------------------------------------------
